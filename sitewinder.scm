4355175b6a48cea357fb55ae089f7f2022e3094a
Pierre De Pascale <pierre.depascale@prefics.ch>
2016-01-31 13:22:54 +0100

    cleaned sitewinder algorithm

--- sitewinder.scm
+++ sitewinder.scm
@@ -1,23 +1,6 @@
 ;;; sitewinder.scm -- sitewinder algorithm for maze generation
 
-#lang racket
-
-(define $width 20)
-
-(define (make-empty-maze) (make-vector (* $width $width) 0))
-(define (maze-ref maze i j) (vector-ref maze (+ (* $width i) j)))
-(define (maze-set! maze i j v) (vector-set! maze (+ (* $width i) j) v))
-(define (fold-maze! proc init maze)
-  (let byrow ((row 0)
-              (init init))
-    (if (< row $width)
-        (let bycol ((col 0)
-                    (init init))
-          (if (< col $width)
-              (let ((next (proc maze row col init)))
-                (bycol (+ col 1) next))
-              (byrow (+ row 1) init)))
-        maze)))
+;#lang racket
 
 (define (sitewinder maze)
   (let lp ((row 0))
@@ -43,9 +26,6 @@
                    (bycell (+ col 1) '()))))
           maze)))
 
-(define (at-east? row col) (= col $width))
-(define (at-bottom? row col) (= row $width))
-
 (define (open-north! maze run)
   (let ((cell (list-ref run (random (length run)))))
     (maze-set! maze (car cell) (cdr cell) (+ (maze-ref maze (car cell) (cdr cell)) 2))))
@@ -53,19 +33,6 @@
 (define (open-east! maze row col)
   (maze-set! maze row col 1))
 
-(define (display-maze maze)
-  (display "+--------------+") (newline)
-  (fold-maze! (lambda (m row col i)
-                (if (= col 0) (newline) 1)
-                (let ((box (maze-ref maze row col)))
-                  (cond ((= box 0) (display "_|"))
-                        ((= box 1) (display "__"))
-                        ((= box 2) (display " |"))
-                        ((= box 3) (display "  "))
-                        (else (error "unknown maze contents " maze row col box)))))
-              #f maze)
-  #f)
-
-(define (test) (display-maze (sitewinder (make-empty-maze))))
+(define (test-sitewinder) (display-maze (sitewinder (make-empty-maze))))
 
 (define m (make-empty-maze))
